ae15883b0f273deb88b0e9e0651072236985e424
Harish1611 <harishnaidub005@gmail.com>
2023-10-05 23:09:11 +0530

    Style and Variables

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,18 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 2, 5);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 300;
+  const locationOfExpenditure = "Security";
   return (
-    <div>
-      <h2> Expense Item!</h2>
-      <h3> Date: 5th October</h3>
-      <p>Food Rs 10</p>
-
-      <p>Petrol Rs 100</p>
-
-      <p>Movies Rs 200</p>
+    <div className="expense-item">
+      <div> {expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2> {expenseTitle}</h2>
+        <h2> {locationOfExpenditure}</h2>
+        <div className="expense-item__price">$${expenseAmount}</div>
+      </div>
     </div>
   );
 }
