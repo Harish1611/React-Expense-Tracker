e6123bdebe0a0bc0c99ff5c68b7b624b1096784e
Harish1611 <harishnaidub005@gmail.com>
2023-10-05 20:41:04 +0530

    First Component Creation

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2> Expense Item!</h2>
+      <h3> Date: 5th October</h3>
+      <p>Food Rs 10</p>
+
+      <p>Petrol Rs 100</p>
+
+      <p>Movies Rs 200</p>
+    </div>
+  );
+}
+
+export default ExpenseItem;
