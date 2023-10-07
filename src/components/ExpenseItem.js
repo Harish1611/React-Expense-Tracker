3696eaf45648dc6679aad79aa551b9dcaf27b561
Harish1611 <harishnaidub005@gmail.com>
2023-10-07 20:48:26 +0530

    Impletemented Date Formate logic, ExpenseItem Component is split into ExpenseDate and ExpenseDetails Components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,16 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 
 function ExpenseItem(expenseItemList) {
-  console.log(expenseItemList);
-
   return (
     <div className="expense-item">
-      <div>
-        <h2> {expenseItemList.name}</h2>
-      </div>
-      <div className="expense-item__description">
-        <h2> {expenseItemList.date.toISOString()}</h2>
-        <h2> {expenseItemList.locationOfExpenditure}</h2>
-        <div className="expense-item__price">${expenseItemList.amount}</div>
-      </div>
+      <ExpenseDate date={expenseItemList.date} />
+      <ExpenseDetails
+        name={expenseItemList.name}
+        locationOfExpenditure={expenseItemList.locationOfExpenditure}
+        amount={expenseItemList.amount}
+      />
     </div>
   );
 }
