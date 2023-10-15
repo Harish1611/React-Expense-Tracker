b414a0120c30e85f7549ad7aebae399c2e9d6706
Harish1611 <harishnaidub005@gmail.com>
2023-10-15 16:38:53 +0530

    Working with useState Hooks & Updated parts of UI components with useState hooks

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,24 +3,33 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 
+import { useState } from "react";
+
 const ExpenseItem = (expenseItemList) => {
+  const [title, setTitle] = useState(expenseItemList.name); //React hook
+
+  const [expenseCost, setExpenseCost] = useState(expenseItemList.amount);
   const deteleExpense = (event) => {
     console.log(event);
   };
   const clickhandler = () => {
+    setTitle("Updated"); // calling and passing the new value
     console.log("clicked");
+  };
+  const expenseAmountHandler = () => {
+    setExpenseCost(100);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={expenseItemList.date} />
       <ExpenseDetails
-        name={expenseItemList.name}
+        name={title}
         locationOfExpenditure={expenseItemList.locationOfExpenditure}
-        amount={expenseItemList.amount}
+        amount={expenseCost}
       />
+      <button onClick={expenseAmountHandler}>Change Amout</button>
       <button onClick={clickhandler}>Change Title</button>
-
       <button onClick={deteleExpense}>Delete</button>
     </Card>
   );
